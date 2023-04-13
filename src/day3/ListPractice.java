299e57ad9989dbfb57168437b38e85af007bf709
Beggie98 <bjuraev2020@gmail.com>
2023-04-13 10:11:08 +0500

    completed the list branch

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -8,6 +8,7 @@ public class ListPractice {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         //adding one item
+        //printing out
     }
 
 }
