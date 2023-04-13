30e3d7653cb34627d236788deac5b487f39e0ba3
Beggie98 <bjuraev2020@gmail.com>
2023-04-13 10:26:22 +0500

    added what Set keeps

--- src/day3/SetPractice.java
+++ src/day3/SetPractice.java
@@ -4,5 +4,6 @@ public class SetPractice {
 
     //Set is a collection type that only stores unique items
     //if you add duplicate it will just remove it
+    //it is a collection of unique elements
 
 }
