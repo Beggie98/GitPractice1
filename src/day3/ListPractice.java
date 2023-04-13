dc87cfe4a0cb534a6f7568166a3fce09c72b1fed
Beggie98 <bjuraev2020@gmail.com>
2023-04-13 10:09:57 +0500

    created new Arraylist

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -1,7 +1,12 @@
 package day3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListPractice {
 
-
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
 
 }
